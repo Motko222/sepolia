7e4a5e2485e00c8cf34487ae45a1fe1ad7b5729d
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-30 23:21:47 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -10,8 +10,10 @@ version=$()
 docker_status=$(docker inspect $CONTAINER | jq -r .[].State.Status)
 local_height=$(( 16#$(curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' $LOCAL_RPC | jq -r .result.currentBlock | sed 's/0x//g') ))
 network_height=$(( 16#$(curl -s POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' $PUBLIC_RPC | jq -r .result.number | sed 's/0x//') ))
+diff=$(( $network_height - $local_height ))
 
-status=warning && message="syncing $local_height/$network_height (behind $(( $network_height - local_height )) )"
+status="ok"
+[ $diff -gt 5 ] && status="warning" && message="syncing $local_height/$network_height (behind $diff )"
 [ $docker_status -ne "running" ] && status="error" message="docker not running ($docker_status)"
 
 cat >$json << EOF
