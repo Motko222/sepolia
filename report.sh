d11b54716a47e41a53c8be471e8c53427405f120
Motko <77466238+Motko222@users.noreply.github.com>
2025-06-02 21:28:12 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -11,7 +11,7 @@ docker_status=$(docker inspect $CONTAINER | jq -r .[].State.Status)
 geth_syncing=$(curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' $URL1 | jq -r .result ) 
 prysm_syncing=$(curl -s $URL2/eth/v1/node/syncing | jq -r .data.is_syncing)
 
-if [ -z $geth_synced ]
+if [ $geth_syncing ]
 then 
  local_height=$(( 16#$(curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' $URL1 | jq -r .result.currentBlock | sed 's/0x//g') ))
  network_height=$(( 16#$(curl -s POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' $URL5 | jq -r .result.number | sed 's/0x//') ))
