503bf12451e165ee034f7116b1ead4622e91c161
Motko <77466238+Motko222@users.noreply.github.com>
2025-06-02 21:26:14 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -8,12 +8,19 @@ source $path/env
 
 version=$()
 docker_status=$(docker inspect $CONTAINER | jq -r .[].State.Status)
-local_height=$(( 16#$(curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' $URL1 | jq -r .result.currentBlock | sed 's/0x//g') ))
-network_height=$(( 16#$(curl -s POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' $URL5 | jq -r .result.number | sed 's/0x//') ))
-diff=$(( $network_height - $local_height ))
+geth_synced=$(curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' $URL1 | grep result | grep -c false )
+prysm_syncing=$(curl -s $URL2/eth/v1/node/syncing | jq -r .data.is_syncing)
+
+if [ $geth_synced -eq 0 ]
+then 
+ local_height=$(( 16#$(curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' $URL1 | jq -r .result.currentBlock | sed 's/0x//g') ))
+ network_height=$(( 16#$(curl -s POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' $URL5 | jq -r .result.number | sed 's/0x//') ))
+ diff=$(( $network_height - $local_height ))
+fi
 
 status="ok"
-[ $diff -gt 5 ] && status="warning" && message="syncing $local_height/$network_height (behind $diff )"
+[ $diff -gt 0 ] && status="warning" && message="geth syncing $local_height/$network_height (behind $diff )"
+[ $prysm_syncing ] && status="warning" && message="prysm syncing"
 [ "$docker_status" != "running" ] && status="error" message="docker not running ($docker_status)"
 
 cat >$json << EOF
